e519dbaab4f48b70ea0987bb3114ea55981547aa
Senya <arsenijholkin02@gmail.com>
2024-11-13 15:47:28 +0300

    funcs update

--- Functions/Prefix-func.cpp
+++ Functions/Prefix-func.cpp
@@ -1,3 +1,27 @@
-//
-// Created by Senya on 06.11.2024.
-//
+#include "iostream"
+#include "string"
+#include "vector"
+
+
+void prefix_function(std::vector<int>& p, const std::string& str) {
+    for (int i = 1; i < str.size(); i++) {
+        int j = p[i - 1];
+        while (j > 0 and str[i] != str[j])
+            j = p[j-1];
+
+        if (str[i] == str[j]) j++;
+        p[i] = j;
+    }
+}
+
+int main() {
+    std::string str;
+    std::cin >> str;
+    std::vector<int> res (str.size(), 0);
+
+    prefix_function(res, str);
+
+    for (auto el : res) std::cout << el << ' ';
+
+    return 0;
+}
