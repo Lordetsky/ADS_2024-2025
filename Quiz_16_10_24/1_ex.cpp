0bae72356fbc03c4afb1a9c08f1ba7a52c03f4ac
Senya <arsenijholkin02@gmail.com>
2024-11-13 15:51:08 +0300

    Quiz done

--- Quiz_16_10_24/1_ex.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Senya on 16.10.2024.
-//
--- Quiz_16_10_24/2_ex.cpp
+++ Quiz_16_10_24/2_ex.cpp
@@ -1,3 +1,40 @@
+#include "iostream"
+
+
+int main() {
+    long long x, i = 1, j = 1, k = 1;
+    bool flag = true;
+    std::cin >> x;
+
+    while (k <= x) {
+        if (i * i < j * j * j) {
+            flag = true;
+            if (k < x) i++;
+        }
+        else if (i * i > j * j * j) {
+            flag = false;
+            if (k < x) j++;
+        }
+        else if (k < x){
+            i++, j++;
+        }
+        k++;
+    }
+
+//    for (int a = 1; a <= i; a++) std::cout << a * a << ' ';
 //
-// Created by Senya on 16.10.2024.
+//    std::cout << '\n';
 //
+//    for (int a = 1; a <= j; a++) std::cout << a * a * a << ' ';
+//
+//    std::cout << '\n';
+
+//    i--, j--;
+
+
+
+    if (flag) std::cout << i * i;
+    else std::cout << j * j * j;
+
+    return 0;
+}
